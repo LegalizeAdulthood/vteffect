6319770d897b3ad343051ee471fa6d588e8f4799
Richard <legalize@xmission.com>
2025-12-09 00:01:52 -0700

    Add github workflow, update CMake build scripts

---
+++ cmake/misc_target.cmake
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Copyright 2025 Richard Thomson
+#
+# Create a Miscellaneous target to organize files in the IDE
+function(misc_target)
+    cmake_parse_arguments(ARG "" "" "FILES" ${ARGN})
+    add_custom_target(Miscellaneous)
+    if(ARG_FILES)
+        target_sources(Miscellaneous PRIVATE ${ARG_FILES})
+    endif()
+endfunction()
+
+# Add files to the Miscellaneous target and group them in the IDE
+function(misc_group_sources group)
+    cmake_parse_arguments(ARG "" "" "FILES" ${ARGN})
+    if(ARG_FILES)
+        target_sources(Miscellaneous PRIVATE ${ARG_FILES})
+        set_property(SOURCE ${ARG_FILES} PROPERTY HEADER_FILE_ONLY TRUE)
+        source_group("${group}" FILES ${ARG_FILES})
+    endif()
+endfunction()
